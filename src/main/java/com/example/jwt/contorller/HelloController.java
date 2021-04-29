9f908962c286d648bc733707df1e720081e5211a
wook101 <ehddnr1022@gmail.com>
2021-04-29 23:20:42 +0900

    HelloController생성

--- src/main/java/com/example/jwt/contorller/HelloController.java
+++ src/main/java/com/example/jwt/contorller/HelloController.java
@@ -1,4 +1,15 @@
 package com.example.jwt.contorller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello");
+    }
+
 }

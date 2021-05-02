8bc2f0414009e4522f4695e2a93176b6696b6070
wook101 <ehddnr1022@gmail.com>
2021-05-03 05:16:15 +0900

    UserController 생성

---
+++ src/main/java/com/example/tutorial/contorller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.tutorial.contorller;
+
+import com.example.tutorial.dto.UserDto;
+import com.example.tutorial.entity.User;
+import com.example.tutorial.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody UserDto userDto){
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<User> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+
+}

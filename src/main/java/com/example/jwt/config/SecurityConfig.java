2bbc687a11df4d44971368488bb92c535626b3b6
wook101 <ehddnr1022@gmail.com>
2021-05-02 15:29:07 +0900

    스프링 시큐리티에서 h2-console접근을 위한 설정추가

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.example.jwt.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    
+    //h2-console하위 요청과, favicon요청은 시큐리티 로직을 수행하지 않고 접근
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**",
+                            "/favicon.ico");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

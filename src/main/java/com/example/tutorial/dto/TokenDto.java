5e359f0de4ad4831f82a0a0e5b3c9dcb864b792f
wook101 <ehddnr1022@gmail.com>
2021-05-02 17:05:58 +0900

    토큰dto 클래스 생성

---
+++ src/main/java/com/example/tutorial/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.example.tutorial.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+
+}
